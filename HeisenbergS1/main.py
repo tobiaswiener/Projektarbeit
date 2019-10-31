328ee29c64d52eda77970b3ad472ac8ca147b934
YOUR_USERNAME <tobiaswiener@icloud.com>
2019-10-31 14:21:33 +0100

    different strings for machine sampler and optimizer added

--- HeisenbergS1/main.py
+++ HeisenbergS1/main.py
@@ -11,6 +11,19 @@ FFNeuralNet = True
 RestrictedBM = False
 graph, hilbert, hamilton = build.generateNN(length=L,coupling=J)
 
+
+machine = ["Jastrow","JastrowSymm","FFNN", "RbmSpin", "RmbSpinSymm"]
+sampler = ["ExactSampler", "MetropolisExchange","MetropolisExchangePt",
+           "MetropolisLocal", "MetropolisLocalPt", "MetropolisHamiltonian",
+           "MetropolisHamiltonianPt", "MetropolisHop"]
+optimizer = ["Sgd","RmsProp", "Momentum", "AmsGrad", "AdaMax", "AdaGrad", "AdaDelta"]
+methode = ["Gd", "Sr"]
+
+n_samples = [1000,2000]
+n_iterations = [500,1000]
+nhlayer = [1,2,3]
+i = 0
+names = []
 if(FFNeuralNet):
     FFNN.runFFNN(graph,hilbert,hamilton)
 if(ED):
