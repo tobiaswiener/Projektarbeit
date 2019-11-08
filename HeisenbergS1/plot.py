82c80a404a4b03c6b0502f93661929e0b4a1ce50
tobiaswiener <tobiaswiener@icloud.com>
2019-11-08 12:14:11 +0100

    nla

--- HeisenbergS1/plot.py
+++ HeisenbergS1/plot.py
@@ -31,30 +31,33 @@ def plotFromLogFile(filename):
     plt.show()
 
 
-def SubPlotFromFile(filename, N, i):
-    data = json.load(open("1stResults/" + filename))
-    exact_gs_energy = -34.46969272725688
-    w = math.floor(math.sqrt(N+1))+1
-    d = math.floor(math.sqrt(N+1))+1
+def SubPlotFromFile(filenameList, folder=""):
+
+    N = len(filenameList)
+
+    for counter, name in enumerate(filenameList):
+        data = json.load(open(folder+name))
+        exact_gs_energy = -34.46969272725688
+        w = math.floor(math.sqrt(N + 1)) + 1
+        d = math.floor(math.sqrt(N + 1)) + 1
+
+        iters = []
+        energy = []
+        for iteration in data["Output"]:
+            iters.append(iteration["Iteration"])
+            energy.append(iteration["Energy"]["Mean"])
+        plt.rcParams.update({'font.size': 4})
+        ax1 = plt.subplot(w, d, counter + 1)
+
+        plt.plot(iters, energy, color='C8', label=name)
+        plt.title(name)
+        ax1.set_ylabel('Energy')
+        ax1.set_xlabel('Iteration')
+        ax1.xaxis.set_visible(False)
+        plt.axis([0, iters[-1], exact_gs_energy - 1, exact_gs_energy + 20])
+        plt.axhline(y=exact_gs_energy, xmin=0,
+                    xmax=iters[-1], linewidth=2, color='k', label='Exact')
 
 
-    iters = []
-    energy = []
-    for iteration in data["Output"]:
-        iters.append(iteration["Iteration"])
-        energy.append(iteration["Energy"]["Mean"])
-    plt.rcParams.update({'font.size': 4})
-    ax1 = plt.subplot(w,d,i+1)
-
-    plt.plot(iters, energy,  color='C8', label=filename)
-    plt.title(filename)
-    ax1.set_ylabel('Energy')
-    ax1.set_xlabel('Iteration')
-    ax1.xaxis.set_visible(False)
-    plt.axis([0,iters[-1],exact_gs_energy-1,exact_gs_energy+20])
-    plt.axhline(y=exact_gs_energy, xmin=0,
-                 xmax=iters[-1], linewidth=2, color='k', label='Exact')
-
-    if(i+1==N):
-        plt.show()
+    plt.show()
 
--- HeisenbergS1/plotAuto.py
+++ HeisenbergS1/plotAuto.py
@@ -7,12 +7,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import plot
-
+import os.path
 
 
 
 testing = True
-L = 6
+folder = "3rdResults/"
 
 if(testing):
     sampler = ["ExactSampler",
@@ -23,6 +23,7 @@ if(testing):
     n_iterations = [7000]
     nhlayer = [3,5]
     fneuron =[3,5,7]
+    L = [8,10,12,14,16,18,20]
 else: #all that were done
     sampler = ["ExactSampler", "MetropolisExchange", "MetropolisExchangePt",
                "MetropolisLocal", "MetropolisLocalPt", "MetropolisHamiltonian",
@@ -36,17 +37,18 @@ else: #all that were done
 
 N=len(sampler)*len(optimizer)*len(nhlayer)*len(fneuron)*len(n_samples)*len(methode)*len(n_iterations)
 i = 0
-for s in sampler:
-    for o in optimizer:
-        for nhl in nhlayer:
-            for fn in fneuron:
-                for n in n_samples:
-                    for m in methode:
-                        for ni in n_iterations:
-                            filename = str(0) + str(L) + "_" + str(fn) + "FFNN" + str(nhl) + "_" + s \
-                                      + str(n) + "_" + str(o) + "_" + str(ni) + "_" + m +".log"
-
-                            plot.SubPlotFromFile(filename, N, i)
-                            i = i + 1
-
+filenameList = []
+for l in L:
+    for s in sampler:
+        for o in optimizer:
+            for nhl in nhlayer:
+                for fn in fneuron:
+                    for n in n_samples:
+                        for m in methode:
+                            for ni in n_iterations:
+                                filename = str(0) + str(L) + "_" + str(fn) + "FFNN" + str(nhl) + "_" + s \
+                                          + str(n) + "_" + str(o) + "_" + str(ni) + "_" + m +".log"
+                                if(os.path.isfile(folder + filename)):
+                                    filenameList.append(filename)
+                                plot.SubPlotFromFile(filenameList,folder)
 
