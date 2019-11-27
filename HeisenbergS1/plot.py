8cb281924abb8d8900e9510452b1dd90dbd3ac4b
tobiaswiener <tobiaswiener@icloud.com>
2019-11-27 13:15:18 +0100

    plot function for v2.1 log files added

--- HeisenbergS1/plot.py
+++ HeisenbergS1/plot.py
@@ -149,14 +149,14 @@ def plot_folder_in_same_plot(folder: str,label:str = "name"):  #legend=["name","
         exact_gs_energy_infinty = EXACT_ENERGY_PER_SITE_L_INFINTY*L
         exact_gs_energy_L10 = EXACT_ENERGY_LANCZOS_L10*L
         with open(folder + "/" + name) as f:
-            line = f.readlines()
-        for line in line:
+            lines = f.readlines()
+        for line in lines:
             try:
                 b = json.loads(line[0:len(line) - 2])
                 data.append(b)
             except ValueError as e:
+                print(counter)
                 pass
-
         iters = []
         energy = []
 
@@ -183,6 +183,84 @@ def plot_folder_in_same_plot(folder: str,label:str = "name"):  #legend=["name","
     plt.axhline(y=exact_gs_energy_L10, xmin=0,
                 xmax=iters[-1], linewidth=2, color='k', label='ExactLanczos')
     #plt.plot(iters,np.zeros_like(iters))
+    plt.title(name)
+    plt.ylabel('Energy')
+    plt.xlabel('Iteration')
+    plt.axis([0, iters[-1], exact_gs_energy_infinty+Y_MIN_From_Exact, exact_gs_energy_infinty+Y_MAX_From_Exact])
+
+    plt.legend()
+    plt.show()
+
+
+def plot_folder_in_same_plot_cluster(folder: str,label:str = "name"):
+
+    filename_list = []
+    for filename in os.listdir(folder + "/"):
+        if filename.endswith(".log"):
+            filename_list.append(filename)
+    number_plots = len(filename_list)
+    all_iters = []
+    all_energy = []
+    all_names = []
+    L = -1
+    machine = "machine"
+    sampler = "sampler"
+    optimizer = "optimizer"
+    VMC = "VMC"
+    for counter, name in enumerate(filename_list):
+        all_names.append(name)
+        data = []
+        input = load.specs_runnable.log_to_input(folder=folder,file_name=name)
+        try:
+            L = input["input"]["L"]
+            machine = input["input"]["machine"]
+            sampler = input["input"]["sampler"]
+            optimizer = input["input"]["optimizer"]
+            VMC = input["input"]["VMC"]
+        except KeyError:
+            print(name + " is not yet finished")
+
+
+        exact_gs_energy_infinty = EXACT_ENERGY_PER_SITE_L_INFINTY*L
+        exact_gs_energy_L10 = EXACT_ENERGY_LANCZOS_L10*L
+
+        with open(folder + "/" + name) as f:
+            lines = f.readlines()
+
+        try:
+            data = json.loads(lines[0])
+        except ValueError as err:
+            print(err)
+
+        iters = []
+        energy = []
+        try:
+            for it in data["Output"]:
+                iters.append(it["Iteration"])
+                energy.append(it["Energy"]["Mean"])
+        except:
+            print(name,"failed")
+
+        if label == "name":
+            plt.plot(iters, energy, label=name)
+        elif label == "machine":
+            plt.plot(iters, energy, label=machine)
+        elif label == "sampler":
+            plt.plot(iters, energy, label=sampler)
+        elif label == "optimizer":
+            plt.plot(iters, energy, label=optimizer)
+        elif label == "VMC":
+            plt.plot(iters, energy, label=VMC)
+
+        all_iters.append(iters)
+        all_energy.append(energy)
+        all_names.append(name)
+    plt.rcParams.update({'font.size': 8})
+    plt.axhline(y=exact_gs_energy_infinty, xmin=0,
+                 xmax=iters[-1], linewidth=2, color='k', label='ExactInfinity')
+    plt.axhline(y=exact_gs_energy_L10, xmin=0,
+                 xmax=iters[-1], linewidth=2, color='blue', label='ExactLanczos')
+    plt.plot(iters,np.zeros_like(iters))
     plt.title(name)
     plt.ylabel('Energy')
     plt.xlabel('Iteration')
