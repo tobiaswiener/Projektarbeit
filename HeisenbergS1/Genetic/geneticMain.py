3daf5566e5403e04be838ca16fede875ee546a17
tobiaswiener <tobiaswiener@icloud.com>
2019-12-05 13:32:12 +0100

    tournament_cluster method implemented

--- HeisenbergS1/Genetic/geneticMain.py
+++ HeisenbergS1/Genetic/geneticMain.py
@@ -1,4 +1,4 @@
-import testmod
+
 import numpy as np
 import math
 import os
@@ -6,11 +6,12 @@ import Individual
 import Population
 import matplotlib.pyplot as plt
 import geneticPlot
+import netket as nk
 
 
 CLUSTER = False  #True,False
 #setting global seed
-_SEED = 123122
+_SEED = 144444
 np.random.seed(_SEED)
 #specify genes
 MAX_NEURONS_PER_LAYER = 64         #must be mod 2
@@ -22,16 +23,15 @@ BIT_LENGTH_HIDDEN_LAYER = int(math.log2(MAX_HIDDEN_LAYERS))
 BIT_LENGTH_CHROMOSOME = BIT_LENGTH_NO_LAYER + BIT_LENGTH_HIDDEN_LAYER
 #reproduction details
 TOURNAMENT_SIZE = 4                 #4
-POPULATION_SIZE = 30                #30
+POPULATION_SIZE = 10                #30
 MUTATE_PROB = 0.01                  #0.01
 SELECTION_METHOD = "tournament"     #tournament, roulette
-
 CROSSOVER_PROP = 0.75               #0.75
 
 
 
 #specify details of network optimization
-L = 18       #6-18
+L = 6       #6-18
 J = 1
 #optimizer
 OPTIMIZER = "AdaMax"
@@ -45,13 +45,13 @@ D_MAX = 5
 #VMC
 DISCARDED_SAMPLES = 100
 DISCARDED_SAMPLES_ON_INIT = 0
-METHOD = "Sr"               #["Gd","Sr"]
-N_SAMPLES = 100
+METHOD = "Gd"               #["Gd","Sr"]
+N_SAMPLES = 1000
 DIAG_SHIFT = 10
 USE_ITERATIVE = True   #[False,True]
 USE_CHOLESKY = True         #[False,True]
 TARGET = "energy"
-N_ITER = 100
+N_ITER = 1000
 
 #exact Solutions
 _EXACT_GS_L6 = -1.020297256150904
@@ -76,7 +76,7 @@ else:
 
 
 #global working directory
-DIRECTORY = "logs/L%d_%d_%d_%s" %(L,MAX_NEURONS_PER_LAYER,MAX_HIDDEN_LAYERS,ACTIVATION_FUNCTION)
+DIRECTORY = "logs/L%d_%d_%d_%s_%s_%s" %(L,MAX_NEURONS_PER_LAYER,MAX_HIDDEN_LAYERS,ACTIVATION_FUNCTION,N_ITER,METHOD)
 if not (os.path.exists(DIRECTORY)):
     os.mkdir(DIRECTORY)
 
@@ -104,7 +104,7 @@ def tournament_plot_all():
     pop = Population.Population.create_random_population()
     pop.print_genes()
 
-    for gen in range(10):
+    for gen in range(100):
         pop.new_generation()
         pop.print_genes()
     fittest_genome = pop.give_fittest_individual().give_genes().bin
@@ -112,10 +112,24 @@ def tournament_plot_all():
     geneticPlot.plot_file(fittest_genome+".log",DIRECTORY)
     print("Possible Networks", 2**BIT_LENGTH_CHROMOSOME)
     print("Calculated Networks: ", len(Individual.CALCULATED_NETWORKS))
+    print(pop.give_fittest_individual().decode_genome())
+
+def tournament_cluster():
+    pop = Population.Population.create_random_population()
+    if(nk.MPI.rank()==0):
+        pop.print_genes()
+    for gen in range(100):
+        pop.new_generation()
+        if (nk.MPI.rank() == 0):
+            pop.print_genes()
+    if(nk.MPI.rank()==0):
+        print("Possible Networks", 2 ** BIT_LENGTH_CHROMOSOME)
+        print("Calculated Networks: ", len(Individual.CALCULATED_NETWORKS))
+
 
 def main():
-    tournament_plot_all()
-
+    tournament_cluster()
+    #tournament_plot_all()
 
 if __name__=="__main__":
     main()
