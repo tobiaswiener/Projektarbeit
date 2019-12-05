be5b67e9a9831cde4019834b7716e5e8149fe10a
YOUR_USERNAME <tobiaswiener@icloud.com>
2019-12-05 07:51:09 +0100

    tournament_plot_all method added

--- HeisenbergS1/Genetic/geneticMain.py
+++ HeisenbergS1/Genetic/geneticMain.py
@@ -5,11 +5,12 @@ import os
 import Individual
 import Population
 import matplotlib.pyplot as plt
+import geneticPlot
 
 
 CLUSTER = False  #True,False
 #setting global seed
-_SEED = 1234
+_SEED = 123122
 np.random.seed(_SEED)
 #specify genes
 MAX_NEURONS_PER_LAYER = 64         #must be mod 2
@@ -21,7 +22,7 @@ BIT_LENGTH_HIDDEN_LAYER = int(math.log2(MAX_HIDDEN_LAYERS))
 BIT_LENGTH_CHROMOSOME = BIT_LENGTH_NO_LAYER + BIT_LENGTH_HIDDEN_LAYER
 #reproduction details
 TOURNAMENT_SIZE = 4
-POPULATION_SIZE = 10
+POPULATION_SIZE = 30
 MUTATE_PROB = 0.01
 SELECTION_METHOD = "tournament"     #tournament, roulette
 
@@ -30,7 +31,7 @@ CROSSOVER_PROP = 0.75
 
 
 #specify details of network optimization
-L = 14
+L = 18
 J = 1
 #optimizer
 OPTIMIZER = "AdaMax"
@@ -50,7 +51,7 @@ DIAG_SHIFT = 10
 USE_ITERATIVE = True   #[False,True]
 USE_CHOLESKY = True         #[False,True]
 TARGET = "energy"
-N_ITER = 1
+N_ITER = 100
 
 #exact Solutions
 _EXACT_GS_L6 = -1.020297256150904
@@ -94,14 +95,26 @@ def tournament_test():
         fitnesslist[0].append(gen + 1)
         fitnesslist[1].append(pop.sum_fitness())
         pop.print_genes()
+
     plt.plot(fitnesslist[0], fitnesslist[1], label="Tournament size" + str(3))
-    pop.print_genes()
+
     print("Total Number Networks: " , len(Individual.CALCULATED_NETWORKS))
 
-    #todo implement plot all and fittest method
+def tournament_plot_all():
+    pop = Population.Population.create_random_population()
+    pop.print_genes()
+
+    for gen in range(10):
+        pop.new_generation()
+        pop.print_genes()
+    fittest_genome = pop.give_fittest_individual().give_genes().bin
+    geneticPlot.plot_folder_in_same_plot(DIRECTORY,"machine")
+    geneticPlot.plot_file(fittest_genome+".log",DIRECTORY)
+    print("Possible Networks", 2**BIT_LENGTH_CHROMOSOME)
+    print("Calculated Networks: ", len(Individual.CALCULATED_NETWORKS))
 
 def main():
-    tournament_test()
+    tournament_plot_all()
 
 
 if __name__=="__main__":
