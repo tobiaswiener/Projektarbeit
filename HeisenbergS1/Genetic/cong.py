184cfcb6f9eaa3b0549ba32758640f1d8ef22609
tobiaswiener <tobiaswiener@icloud.com>
2019-12-12 07:28:29 +0100

    SEED.txt added to git

--- HeisenbergS1/Genetic/cong.py
+++ HeisenbergS1/Genetic/cong.py
@@ -6,4 +6,4 @@ seeds = np.random.randint(1,9999999,size=nk.MPI.size())
 # with futures.ProcessPoolExecutor(max_workers=32) as executors:
 #     wait_for = [executors.submit(geneticMain.test_hyper, seed) for seed in seeds]
 geneticMain.test_hyper(seeds[nk.MPI.rank()])
-print(str(nk.MPI.rank())+"-"+str(seeds[nk.MPI.rank()]))
+
